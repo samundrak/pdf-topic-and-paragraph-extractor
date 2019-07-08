412b8b819fe810f2504343b98f4b72036311622a
samundrak <samundrak@yahoo.com>
2019-07-08 23:35:39 +0545

    investigating pdf.js

--- index.js
+++ index.js
@@ -1,75 +1,59 @@
-let fs = require('fs');
-const PDFParser = require('pdf2json');
-const path = require('path');
-var pdf_extract = require('pdf-extract');
-var absolute_path_to_pdf = path.join(__dirname, 'pdf.pdf');
-var options = {
-  type: 'text', // perform ocr to get the text within the scanned image
-};
+const fs = require('fs');
+const pdf = require('pdf-parse');
 
-var processor = pdf_extract(
-  '/home/samundrak/Documents/ books/javascript-the-good-parts.pdf',
-  options,
-  function(err) {
-    if (err) {
-      console.log(err);
-      // return callback(err);
-    }
-  }
+let dataBuffer = fs.readFileSync(
+  '/home/samundrak/Documents/ books/javascript-the-good-parts.pdf'
 );
 
-const paragraphsMap = new Map();
-const pages = new Map();
-let paraCounter = 0;
-const topics = [];
-const ebookTree = {};
-
-const isLineTopic = (line, para) => {
-  if (line.endsWith('.')) {
-    return false;
-  }
-
-  if (line.length > 50) {
-    return false;
-  }
-  return true;
-};
-const parseParagraph = (paragraph) => {
-  const para = paragraph.split(/\n/);
-  let topic = para[0];
-  const lineLengths = para.map((item) => item.length);
-  const maxLine = Math.max(lineLengths);
-  const isTopic = isLineTopic(topic);
-  if (!isTopic) {
-    topic = null;
-  }
-  return {
-    topic,
-    content: isTopic ? para.slice(1, para.length).join('\n') : para.join('\n'),
+const renderPage = function(pageData) {
+  //check documents https://mozilla.github.io/pdf.js/
+  let render_options = {
+    //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
+    normalizeWhitespace: false,
+    //do not attempt to combine same line TextItem's. The default value is `false`.
+    disableCombineTextItems: false,
   };
-};
-
-processor.on('complete', function(data) {
-  data.text_pages.slice(97, 98).forEach((page, pageNumber) => {
-    console.log(page);
-    // const paragraphs = page.split(/\n\n\n?\n?\n?/);
-    // const paragraphIndexes = [];
-    // paragraphs.forEach((paragraph) => {
-    //   const parsedParagraph = parseParagraph(paragraph);
-    //   if (parsedParagraph.topic) {
-    //     topics.push({
-    //       topicIndexedAt: paraCounter,
-    //       topic: parsedParagraph.topic,
-    //     });
-    //   }
-    //   paragraphsMap.set(paraCounter, parsedParagraph);
-    //   paragraphIndexes.push(paraCounter);
-    //   paraCounter++;
-    // });
-    // pages.set(pageNumber, paragraphIndexes);
+  //   console.log(pageData);
+  return pageData.getTextContent(render_options).then(function(textContent) {
+    // if (pageData.pageIndex === 2 || pageData.pageIndex === 81) {
+    const mapByTransform = new Map();
+    textContent.items.forEach((element) => {
+      const { str, transform } = element;
+      if (!mapByTransform.has(transform[0])) {
+        mapByTransform.set(transform[0], [str]);
+      } else {
+        mapByTransform.get(transform[0]).push(str);
+      }
+    });
+    console.log(mapByTransform);
+    // }
+    //     let lastY;
+    //     let text = '![pageStart]';
+    //     for (let item of textContent.items) {
+    //       if (lastY == item.transform[5] || !lastY) {
+    //         text += item.str;
+    //       } else {
+    //         text += '\n' + item.str;
+    //       }
+    //       lastY = item.transform[5];
+    //     }
+    //     return text + '![pageEnd]';
   });
-
-  console.log(pages);
-  console.log(paragraphsMap);
-  console.log(topics);
+};
+pdf(dataBuffer, {
+  pagerender: renderPage,
+}).then(function(data) {
+  //   // number of pages
+  //   console.log(data.numpages);
+  //   // number of rendered pages
+  //   console.log(data.numrender);
+  //   // PDF info
+  //   console.log(data.info);
+  //   // PDF metadata
+  //   console.log(data.metadata);
+  //   // PDF.js version
+  //   // check https://mozilla.github.io/pdf.js/getting_started/
+  //   console.log(data.version);
+  // PDF text
+  //   console.log(data.text);
 });
